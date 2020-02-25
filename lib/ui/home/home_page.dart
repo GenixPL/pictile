9b4b9ab2390bf20298bacd2f094da49d1e185573
genix <intergenixyt@gmail.com>
2020-02-25 13:42:36 +0100

    Clean home

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:pictile/main.dart';
 import 'package:pictile/navigation/routes.dart';
 import 'package:pictile/ui/common/app_text_style.dart';
 import 'package:pictile/ui/common/basic_page.dart';
@@ -9,81 +7,78 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BasicPage(
-      showBackArrow: false,
-      showWhiteBackground: false,
       children: <Widget>[
         Expanded(
-          child: IntrinsicWidth(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Text(
-                      "The point isn't to win the game. The point is to play a beautiful game.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontFamily: 'RobotoSlab',
-                        color: Colors.white,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                      ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: IntrinsicWidth(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: _buildQuote()),
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        _buildHomeButton(
+                          text: 'SHOW',
+                          onTap: () => Navigator.pushNamed(
+                            context,
+                            showMenuRoute,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        _buildHomeButton(
+                          text: 'MANAGE',
+                          onTap: () => Navigator.pushNamed(
+                            context,
+                            manageMenuRoute,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      OutlineButton(
-                        child: Text('SHOW', style: whiteTextStyle),
-                        splashColor: Colors.white,
-                        highlightedBorderColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32),
-                        ),
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 2,
-                        ),
-                        padding: EdgeInsets.all(0),
-                        onPressed: () => Navigator.pushNamed(
-                          context,
-                          showMenuRoute,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      OutlineButton(
-                        child: Text('MANAGE', style: whiteTextStyle),
-                        splashColor: Colors.white,
-                        highlightedBorderColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32),
-                        ),
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 2,
-                        ),
-                        padding: EdgeInsets.all(0),
-                        onPressed: () => Navigator.pushNamed(
-                          context,
-                          manageMenuRoute,
-                        ),
-                      ),
-//                      RaisedButton(
-//                        child: Text('REMOVE DB'),
-//                        onPressed: () async => await db.deleteDb(),
-//                      ),
-                    ],
-                  ),
-                ),
-                Expanded(child: Container()),
-              ],
+                  Expanded(child: Container()),
+                ],
+              ),
             ),
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildQuote() {
+    final text =
+        '"The point isn\'t to win the game. The point is to play a beautiful game."';
+
+    return Center(
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontFamily: 'RobotoSlab',
+          fontWeight: FontWeight.w500,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHomeButton({String text, Function() onTap}) {
+    return OutlineButton(
+      child: Text(text, style: blackSmallTextStyle),
+      splashColor: Colors.white,
+      highlightedBorderColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(32),
+      ),
+      borderSide: BorderSide(
+        width: 2,
+      ),
+      padding: EdgeInsets.all(0),
+      onPressed: onTap,
     );
   }
 }
